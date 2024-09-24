3e64f6c8ba9ed4efa9c236b518a410e2b45b9eea
yannmart <yann@shib.io>
2024-09-24 10:32:06 +0000

    changes for Shib network

--- scripts/fund_test_addresses_docker.sh
+++ scripts/fund_test_addresses_docker.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
-docker exec -i zama-dev-fhevm-validator-1 faucet a5e1defb98efe38ebb2d958cee052410247f4c80
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet fCefe53c7012a075b8a711df391100d9c431c468
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet a44366bAA26296c1409AD1e284264212029F02f1
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet c1d91b49A1B3D1324E93F86778C44a03f1063f1b
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet 305F1F471e9baCFF2b3549F9601f9A4BEafc94e1
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet 8B8f5091f8b9817EF69cFC1E8B2f721BafF60DF4
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet 03734276e8f8ab253ff4295e66228DAC936FF5b8
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet 9FE8958A2920985AC7ab8d320fDFaB310135a05B
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet 466f26442DD182C9A1b018Cd06671F9791DdE8Ef
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet c45994e4098271c3140117ebD5c74C70dd56D9cd
-sleep 8
-docker exec -i zama-dev-fhevm-validator-1 faucet 97F272ccfef4026A1F3f0e0E879d514627B84E69
+# Define the path to the faucet.py script
+FAUCET_SCRIPT="/home/ubuntu/faucet/faucet.py"
+
+# List of wallet addresses
+addresses=(
+  "a5e1defb98efe38ebb2d958cee052410247f4c80"
+  "fCefe53c7012a075b8a711df391100d9c431c468"
+  "a44366bAA26296c1409AD1e284264212029F02f1"
+  "c1d91b49A1B3D1324E93F86778C44a03f1063f1b"
+  "305F1F471e9baCFF2b3549F9601f9A4BEafc94e1"
+  "8B8f5091f8b9817EF69cFC1E8B2f721BafF60DF4"
+  "03734276e8f8ab253ff4295e66228DAC936FF5b8"
+  "9FE8958A2920985AC7ab8d320fDFaB310135a05B"
+  "466f26442DD182C9A1b018Cd06671F9791DdE8Ef"
+  "c45994e4098271c3140117ebD5c74C70dd56D9cd"
+  "97F272ccfef4026A1F3f0e0E879d514627B84E69"
+  "a5e1defb98efe38ebb2d958cee052410247f4c80"
+)
+
+# Iterate over the wallet addresses and execute the faucet.py script
+for address in "${addresses[@]}"; do
+  python3 "$FAUCET_SCRIPT" faucet "$address"
+  sleep 15
+done
